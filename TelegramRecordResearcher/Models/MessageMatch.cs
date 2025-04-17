9723d49336cc30f914d50a35cda1308fe3cb0f41
Salman Tariq <salmantariqshaykh001@gmail.com>
2025-04-17 10:26:33 +0500

    Add files via upload

---
+++ TelegramRecordResearcher/Models/MessageMatch.cs
@@ -0,0 +1,8 @@
+﻿namespace TelegramRecordSearcher.Models;
+
+public class MessageMatch
+{
+    public string Content { get; set; } = string.Empty;
+    public DateTime Date { get; set; }
+    public string User { get; set; } = string.Empty;
+}
---
+++ TelegramRecordResearcher/Services/TelegramSearchHandler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+class TelegramSearchService
+{
+    private readonly ITelegramBotClient _botClient;
+
+    public TelegramSearchService(string botToken)
+    {
+        _botClient = new TelegramBotClient(botToken);
+    }
+
+    public async Task<List<Message>> GetAllPostsAsync(string channelName)
+    {
+        var posts = new List<Message>();
+
+        var updates = await _botClient.GetUpdates(offset: 0, limit: 100, timeout: 0);
+
+        foreach (var update in updates)
+        {
+            if (update.ChannelPost != null && "@" + update.ChannelPost.Chat.Username == channelName)
+            {
+                posts.Add(update.ChannelPost);
+            }
+        }
+
+        return posts;
+    }
+
+    public async Task<List<Message>> SearchChannelAsync(string channelName, string searchString)
+    {
+        var results = new List<Message>();
+
+        var posts = await GetAllPostsAsync(channelName);
+
+        foreach (var post in posts)
+        {
+            if (post.Text != null && post.Text.Contains(searchString, StringComparison.OrdinalIgnoreCase))
+            {
+                results.Add(post);
+            }
+        }
+
+        return results;
+    }
+}
